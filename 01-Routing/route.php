33c7a34d81be7eedf66d151c6ecc15ab1d9accad
allecosta <alle_costa10@outlook.com>
2023-01-30 15:34:13 -0300

    Route parameters

--- 01-Routing/route.php
+++ 01-Routing/route.php
@@ -1,32 +1,77 @@
 <?php
 
 /**
- * Rota básica que contém uma URI e uma closure.
+ * BASIC ROUTING 
  * 
- * Retornando a string Olá Mundo Laravel!!
  */
+
+// Rota básica que contém uma URI e uma closure
+// Retornando a string Olá Mundo Laravel
+// Rota apontando para o controlador UserController que contém o método index.
+
 Route::get('/', function () {
-    return "Olá Mundo Laravel!!";
+    return view('welcome')
+});
+
+Route::get('/', function () {
+    return "Olá Mundo Laravel";
 });
 
 
-
-/**
- * Esta rota está apontando para o controlador UserController que contém o método index.
- */
 Route::get('/user', [UserController::class, 'index']);
 
 
+// Rota de redirecionamento para outra URI   
+// Personalizando o status code utilizando um terceiro parâmetro (por padrão é 302)
+// Ou utilizando o método permanentRedirect() para retornar o status code 301.
 
-/**
- * Rota de redirecionamento para outra URI 
- * 
- * Personalizando o status code utilizando um terceiro parâmetro (por padrão é 302)
- * 
- * Ou utilizando o método permanentRedirect para retornar o status code 301.
- */
 Route::redirect('/abc', '/xyz');
 
 Route::redirect('/abc', '/xyz', 301);
 
 Route::permanentRedirect('/abc', ('/xyz'));
+
+
+
+/**
+ * ROUTE PARAMETERS 
+ * 
+ */
+
+// Passando parâmetros na rota
+// Capturando o id de um usúario via URL
+// Paasando mais de um parâmetro via URL
+// Passando valor de parâmetro opcional
+// Restrigindo o formato dos parâmetros utilizando o método where() utilizando expressões regulares.
+
+Route::get('/home', function () {
+    return "Olá Mundo Laravel. Bem vindo a nossa página oficial!!";
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "usário: " . $id;
+});
+
+Route::get('/posts/{posts}/comments/{comments}', function ($posts, $comments) {
+
+});
+
+
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
+
+Route::get('user/{name}', function ($name) {
+
+})->where('name', '[A-Za-z]+');
+
+
+Route::get('user/{id}', function ($id) {
+
+})->where('id', '[0-9]+');
+
+
+Route::get('user/{id}/{name}', function ($id, $name) {
+
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
