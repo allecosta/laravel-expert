816c4130abb62a3299dcbdf79df4305c1488b751
allecosta <alle_costa10@outlook.com>
2023-02-13 16:13:24 -0300

    Creating views

---
+++ 03-views/01-Creating/greeting.blade.php
@@ -0,0 +1,8 @@
+{{-- Views são arquivos blade.php (template engine) que contém HTML  --}}
+{{-- Servem para separar a lógica do controlador, da lógica de apresentação. --}}
+
+<html>
+    <body>
+        <h1>Olá, {{ $name }}</h1>
+    </body>
+</html>
